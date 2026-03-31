4da86bac6421c9e8659b6d8ed6310e845f388cfc
Shubham Madane <shubhammadane@gmail.com>
2026-03-31 06:24:04 +0530

    Call API

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
-
+import json
 app = FastAPI()
+
+def load_data():
+    with open("patients.json", "r") as f:
+        data = json.load(f)
+    return data
 
 @app.get("/")
 def hello():
-    return {"message": "Hi Shubham, Welcome to FastAPI!"}
+    return {"message": "Patients management system API is running!"}
 
 @app.get("/shubham")
-def greet():
-    return {"msg": "Hello Shubham, How are you?"}
+def about():
+    return {"msg": "A Fully functional patients management system API built using FastAPI and MongoDB."}
+
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return   data  
+   
